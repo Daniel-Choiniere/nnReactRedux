65e8e4304846c8e092fce9c1433fbba94883f085
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-03-14 10:56:34 +0000

    looping through (mapping) modular data and outputting it in a list with React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,20 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1  },
+      { name: 'Dan', age: 33, belt: 'green', id: 2  },
+      { name: 'Lyli', age: 7, belt: 'purple', id: 3  }
+    ]
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>My first React App!</h1>  
         <p>Welcome brah</p>
-        <Ninjas name="Ryu" age="25" belt="black"/>
-        <Ninjas name="Dan" age="33" belt="green"/>
+        <Ninjas ninjas={ this.state.ninjas }/>
       </div>
     );
   }
